15176fd66e00140eeab2d20f7c510191e4f77071
Pankaj Agarwal; p.agarwal@duke.edu; +1 919 681 6573 <agarw005@plp-lyerlab1.dhe.duke.edu>
2016-04-11 13:52:29 -0400

    Updatd to take in cell line name from cmd line arg and process appropriate file and output result with cell line name in the output file name

--- peptideMS/src/ProcessMSPepSearchResults.R
+++ peptideMS/src/ProcessMSPepSearchResults.R
@@ -1,14 +1,48 @@
 library(stringr)
+#
+# Get the cell line name from cmd line
+#
+cmdFlag = T # change to F if hard coding cell line name in code.
+if(cmdFlag==T){
+args = commandArgs(trailingOnly=TRUE)
+numargs = length(args)
+print(paste("Number of cmd line args",numargs))
+if(length(args)==0) {
+stop("At least one argument must be supplied (input file).tsv", call.=FALSE)
+}else if (length(args)==1) {
+cellLineName = args[1]
+}else{
+stop("Only one argument must be supplied (Cell Line Name)", call.=FALSE)
+}
+#
+}else{#default cell line name
+cellLineName = "MDA231"
+}
+print(paste("cellLineName", cellLineName))
+#
+#
+if(cellLineName=="MDA231"){
+	cellLinePepHitFileName = "MDA231-2.txt";
+} else if(cellLineName=="MCF7"){
+	cellLinePepHitFileName = "MCF7.txt";
+} else if(cellLineName=="MCF10"){
+        cellLinePepHitFileName = "MCF10.txt";
+} else if(cellLineName=="Xenograft"){
+        cellLinePepHitFileName = "Xenograft.txt";
+}else{
+stop("Incorrect cell line name");
+}
+#
 #results dir
 resultsdir = "/srv/agarw005/projects/GeneFusion/analysis/TCGA_TNBC/rna-seq/147files/FusionCatcher_10-09-15/analyze_fusioncatcher_prod_10-18-15/pipeline/genefusion-tcgatnbc-fusioncatcher/peptideMS/results/"
 #
 dfAllFusion = read.table("/srv/agarw005/projects/GeneFusion/analysis/TCGA_TNBC/rna-seq/147files/FusionCatcher_10-09-15/analyze_fusioncatcher_prod_10-18-15/pipeline/genefusion-tcgatnbc-fusioncatcher/peptideMS/data/TN_fusion_032116.txt", sep="\t", stringsAsFactors=F, header=T)
 str(dfAllFusion)
-dfMDA231 = read.table("/srv/agarw005/projects/GeneFusion/analysis/TCGA_TNBC/rna-seq/147files/FusionCatcher_10-09-15/analyze_fusioncatcher_prod_10-18-15/pipeline/genefusion-tcgatnbc-fusioncatcher/peptideMS/data/pepsearch040516/MDA231-2.txt", sep="\t", stringsAsFactors=F, header=T)
-str(dfMDA231)
-dfMDA231 = dfMDA231[,c("Protein.Group.Accessions", "Sequence")]
-str(dfMDA231)
-npeps = nrow(dfMDA231)
+dfCellLine = read.table(paste("/srv/agarw005/projects/GeneFusion/analysis/TCGA_TNBC/rna-seq/147files/FusionCatcher_10-09-15/analyze_fusioncatcher_prod_10-18-15/pipeline/genefusion-tcgatnbc-fusioncatcher/peptideMS/data/pepsearch040516/", cellLinePepHitFileName, sep=""), sep="\t", stringsAsFactors=F, header=T)
+str(dfCellLine)
+dfCellLine = dfCellLine[,c("Protein.Group.Accessions", "Sequence")]
+str(dfCellLine)
+npeps = nrow(dfCellLine)
 npeps
 #
 #
@@ -19,18 +53,18 @@ getPep = function(fusionid){
 }
 ##
 ##
-dfmaster = data.frame(FusionID = character(), HitSequence = character(), FullPepSequence = character(), StartPos = numeric(), EndPos = numeric(), stringsAsFactors=F) #create empty data frame with col header name and data type specify - GOOD
+dfmaster = data.frame(CellLine = character(), FusionID = character(), HitSequence = character(), FullPepSequence = character(), StartPos = numeric(), EndPos = numeric(), stringsAsFactors=F) #create empty data frame with col header name and data type specify - GOOD
 str(dfmaster)
 ##
 ##
 for(i in 1:npeps){
-  #  print(dfMDA231[i,1]);
-  #  print(dfMDA231[i,"Sequence"]);
+  #  print(dfCellLine[i,1]);
+  #  print(dfCellLine[i,"Sequence"]);
   print("START FOR LOOP NEW ITERATION"); 
-  ids = dfMDA231[i,1];
+  ids = dfCellLine[i,1];
   #print(ids)
   #Convert peptide sequence that was reported as MS hit to upper case in case there are lower case letters ("m" is lower case at least)
-  seq = toupper(dfMDA231[i,"Sequence"]);
+  seq = toupper(dfCellLine[i,"Sequence"]);
   qrystrlength = nchar(seq);
   print(paste("qrystrlength", qrystrlength));
   locssep = str_locate_all(pattern=";", ids);
@@ -61,7 +95,7 @@ for(i in 1:npeps){
      ## 
      #dfmaster = rbind(dfmaster, data.frame(splitids[j], seq, stringsAsFactors=F));# can also specify the header col ("FusionID" = ids, "HitSequence" = seq)
      ###dfmaster = rbind(dfmaster, data.frame(ids, seq, stringsAsFactors=F));# can also specify the header col ("FusionID" = ids, "HitSequence" = seq)
-     dfmaster = rbind(dfmaster, data.frame(FusionID=ids, HitSequence=seq, FullPepSequence=targetstring, StartPos=qryloctargetStartPos, EndPos=qryloctargetEndPos, stringsAsFactors=F));# can also specify the header col ("FusionID" = ids, "HitSequence" = seq)
+     dfmaster = rbind(dfmaster, data.frame(CellLine=cellLineName, FusionID=ids, HitSequence=seq, FullPepSequence=targetstring, StartPos=qryloctargetStartPos, EndPos=qryloctargetEndPos, stringsAsFactors=F));# can also specify the header col ("FusionID" = ids, "HitSequence" = seq)
    }
  } else {
    #row=c(ids, seq);
@@ -92,9 +126,9 @@ for(i in 1:npeps){
 	print(qryloctargetEndPos);
 	print("####");
      ## 
-   dfmaster = rbind(dfmaster,data.frame(FusionID=ids, HitSequence=seq, FullPepSequence=targetstring, StartPos=qryloctargetStartPos, EndPos=qryloctargetEndPos, stringsAsFactors=F));# can also specify the header col ("FusionID" = ids, "HitSequence" = seq)
+   dfmaster = rbind(dfmaster,data.frame(CellLine=cellLineName, FusionID=ids, HitSequence=seq, FullPepSequence=targetstring, StartPos=qryloctargetStartPos, EndPos=qryloctargetEndPos, stringsAsFactors=F));# can also specify the header col ("FusionID" = ids, "HitSequence" = seq)
  }
   print("END FOR LOOP ITERATION");
 }
 str(dfmaster)
-write.table(dfmaster, paste(resultsdir,"PepHits_041116_test.tsv",sep=""), row.names=F, col.names=T, sep="\t", quote=F)
+write.table(dfmaster, paste(resultsdir, cellLineName, "_Hits_FusionEvents.tsv",sep=""), row.names=F, col.names=T, sep="\t", quote=F)
